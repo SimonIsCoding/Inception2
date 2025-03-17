a6dd93fa86c4da5d11518acbe337a5d69811f92e
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-03-17 16:26:44 +0100

    struggling with Setting up the makefile

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,6 +4,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=wpuser --admin_password=password --admin_email=simarcha@42.fr --allow-root
 
 php-fpm7.4 -F
