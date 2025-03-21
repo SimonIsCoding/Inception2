a40a86a6a4784ab80bfb9e41e577fa42bbeebd3a
SimArch21 <simon.archambault@hotmail.fr>
2025-03-21 14:49:41 +0100

    updating with hidden passwords. Is it working?

--- srcs/requirements/bonus/fail2ban/ddos_simulation.sh
+++ srcs/requirements/bonus/fail2ban/ddos_simulation.sh
@@ -3,7 +3,7 @@
 # Nombre de requêtes à envoyer (simuler un DDoS)
 NUM_REQUESTS=1000
 # URL de ton site (exemple: login.42.fr)
-URL="http://simarcha.42.fr"
+URL="http://$DOMAIN_NAME"
 
 # Utilisation de curl pour envoyer des requêtes répétées
 for i in $(seq 1 $NUM_REQUESTS); do
--- srcs/requirements/bonus/ftp/script.sh
+++ srcs/requirements/bonus/ftp/script.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+
+mkdir -p /var/www/html
+
+# Créer le répertoire sécurisé requis par vsftpd
+mkdir -p /var/run/vsftpd/empty
+
+# Ajouter un utilisateur FTP avec accès à /var/www/html
+useradd -m -d /var/www/html -s /bin/bash "${FTP_USER}"
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+
+#We give the permissions to FTP_USER to write, read and execute on the html folder
+#Very important if you want to use Filezilla and transfer files 
 chmod 757 /var/www/html/.
+
 /usr/sbin/vsftpd /etc/vsftpd.conf
