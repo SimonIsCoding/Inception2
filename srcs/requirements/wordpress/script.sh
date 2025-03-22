7d944a3d4160a6de86c03c53f091b2f66b00157b
SimArch21 <simon.archambault@hotmail.fr>
2025-03-22 18:31:25 +0100

    still struggling with /static_website redirection

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,6 +4,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=simarcha@42.fr --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$ADMIN_MAIL --allow-root
 
 php-fpm7.4 -F
