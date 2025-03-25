a099c8eb495bc8c1a8f09acfe61ea9fd69dd46e8
simonIsCoding <108285719+simonIsCoding@users.noreply.github.com>
2025-03-25 12:23:53 +0100

    static_website works with domain_name/static redirection. Thanks god (and IA)

---
+++ srcs/requirements/bonus/static_website/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Servir les fichiers statiques depuis le dossier "public"
+app.use(express.static('public'));
+
+app.use('/static', express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur http://localhost:${PORT}`);
+});
