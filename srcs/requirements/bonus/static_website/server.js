bebe508b11887d3c1580c8cf7ab457005320fb46
SimArch21 <simon.archambault@hotmail.fr>
2025-03-30 22:41:28 +0200

    now it's working with the correct container's name, and cadvisor is well redirected with the url. But the volumes names are not well listed => volumes name to change

--- srcs/requirements/bonus/static_website/server.js
+++ srcs/requirements/bonus/static_website/server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 // Servir les fichiers statiques depuis le dossier "public"
 app.use(express.static('public'));
 
-app.use('/static_website', express.static('public'));
+app.use('/static', express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Serveur en écoute sur http://localhost:${PORT}`);
